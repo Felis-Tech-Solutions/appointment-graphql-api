f1e2f4cedbf2b5cfbd56ebbebe2effac9c5660ee
MaxFelis <m.felis@flooris.nl>
2024-03-12 21:14:45 +0100

    Revert "Merge pull request #3 from Felis-Tech-Solutions/86bxhnv3n/feature-appoinments"
    This reverts commit 47edd966acf772e0fb10a56d3a361483ad712f12, reversing
    changes made to fde4343bceda160cdd0b647a416b635a3e7379dd.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,7 @@
 
 namespace Database\Seeders;
 
- use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Models\User;
-use App\Models\Group;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,8 +12,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Group::factory(10)->create()->each(function ($group) {
-            $group->users()->attach(User::factory(3)->create());
-        });
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
